bc43d6799da3e21fb7fb93c3b9993d633cd006c2
hutudanvip <hutudanvip@126.com>
2014-04-03 22:18:46 +0800

    x

--- MyTools/src/main/test/java/com/common/test/Test.java
+++ MyTools/src/main/test/java/com/common/test/Test.java
@@ -1,9 +1,14 @@
 package com.common.test;
 
+import java.util.Date;
+
+import com.common.util.date.DateTimeUtils;
+
 import junit.framework.TestCase;
 
 public class Test extends TestCase {
 	public void testJava() {
 		System.out.println("hello");
+		System.out.println(DateTimeUtils.getDateOfPreNMonth(2, new Date()));
 	}
 }
